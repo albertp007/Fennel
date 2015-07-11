76bb2701adb9c2f9e317b388ed5335b81e7ecae1
Albert Pang <albert.pang@me.com>
2015-07-11 17:00:34 +0800

    Added module YahooFinance to download historical prices from Yahoo Finance Depends on FSharp.Data.dll assembly

---
+++ YahooFinance.fs
@@ -0,0 +1,34 @@
+module YahooFinance
+
+/// This is a private binding for a string print formatter to create the url
+/// to retrieve historical prices from Yahoo Finance
+let private urlFormat = sprintf "http://ichart.yahoo.com/table.csv?s=%s&a=%d\
+&b=%d&c=%d&ignore=.csv"
+
+/// This is a private binding for the CsvProvider type to use to Parse
+/// csv output returned from Yahoo Finance historical prices url.  CsvProvider
+/// depends on the FSharp.Data.dll assembly
+type private Downloader = FSharp.Data.CsvProvider<"Date(date), Open(float), \
+High(float), Low(float), Close(float), Volume(int), \"Adj Close\"(float)">
+
+/// <summary>This function creates the URL for downloading historical prices
+/// from Yahoo Finance given the stock code (Reuters code) and the start date
+/// which is of format <c>yyyy-mm-dd</c></summary>
+/// <param name="stockCode">Reuters stock code of the stock</param>
+/// <param name="startDate">String in <c>yyyy-mm-dd</c> format representing
+/// the start date</param>
+/// <returns>the URL as a string</returns>
+let makeUrl stockCode startDate =
+    let dt = System.DateTime.Parse startDate
+    urlFormat stockCode (dt.Month - 1) dt.Day dt.Year
+
+/// <summary>This function downloads historical prices using a CsvProvider with
+/// schema representing the csv output from Yahoo Finance</summary>
+/// <param name="stockCode">Reuters code of the stock</param>
+/// <param name="startDate">String in <c>yyyy-mm-dd</c> format representing the
+/// start date</param>
+/// <returns>CsvProvider object with the date, open, high, low, close, volume
+/// and adjusted close prices in each row</returns>
+let downloadHist stock dateString =
+    let url = makeUrl stock dateString
+    Downloader.Load url
