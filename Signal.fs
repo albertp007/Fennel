6f6aa9a06ace2b2470eaa5be1915fe23377fa93b
Albert Pang <albert.pang@me.com>
2015-07-28 23:01:26 +0800

    Revamped the filter function to use the Seq.windowed function which simplifies things a lot Changed some of the more generic mathematical functions to use Seq instead of List Added a generic filterByFunc function to calculate other quantities by applying a function on the moving window

--- Signal.fs
+++ Signal.fs
@@ -32,48 +32,6 @@ module Signal =
         | None -> findPatternHelp f acc (n+1) t
     findPatternHelp f [] 0 augmented
 
-  /// <summary>This function calculates the simple moving average of a list of
-  /// float values</summary>
-  /// <param name="n">Size of the moving window</param>
-  /// <param name="lst">The list of float values</param>
-  /// <returns>The first n-1 values will be 0. The values onwards are the
-  /// moving average calculated at the time point n</returns>
-  ///
-  let movingAverage n lst =
-    //
-    // Helper function
-    // i - keep track of the first n iterations, when the queue is not full
-    // q - a queue to keep track of the last n elements
-    // total - current total of all the elements in q
-    // acc - the result
-    //
-    let rec movingAverage' n lst i q total acc =
-      match lst with
-      | [] -> List.rev acc
-      | h::t ->
-        if i < n then
-          //
-          // total' is the new total with a new element from the list
-          // since i < n, the queue is not yet full and therefore we
-          // simply add h to total
-          //
-          let total' = total + h
-          let tmp = if i = n - 1 then total'/(float n) else 0.0
-          movingAverage' n t (i+1) (h::q) total' (tmp::acc)
-        else
-          //
-          // the queue is already full, therefore we reverse the queue
-          // in order to pop the oldest element in the queue and push
-          // the newest element onto it by cons-ing h with the reverse
-          // of the tail part qt
-          let qh::qt = List.rev q
-
-          // the new total is the old total - the oldest element + h
-          let total' = total - qh + h
-          let avg = total'/(float n)
-          movingAverage' n t n (h::(List.rev qt)) total' (avg::acc)
-    movingAverage' n lst 0 [] 0.0 []
-
   /// <summary>This function calculates the dot product of two lists of
   /// float numbers </summary>
   /// <param name="v1">the first vector of floats</param>
@@ -81,7 +39,23 @@ module Signal =
   /// <returns>The dot product of v1 and v2</returns>
   ///
   let dot v1 v2 =
-    List.fold (fun a (x, y)->a + x*y) 0.0 (List.zip v1 v2)
+    Seq.fold (fun a (x, y)->a + x*y) 0.0 (Seq.zip v1 v2)
+
+  /// <summary>produces a list of lists which is the n-slot moving window of a
+  /// sequence of float.  Then apply the function f to each list of n elements
+  /// which are the elements in that window.  The resulting list is prepended
+  /// with (n-1) zeroes to make it the same length of the original sequence
+  /// </summary>
+  /// <param name="n">length of the window</param>
+  /// <param name="f">function to apply to each window</param>
+  /// <returns>sequence of length equal to n, with the first n-1 elements
+  /// being 0.0; thereafter, the element is the result of applying f to each
+  /// window</returns>
+  ///
+  let filterByFunc n f lst =
+    lst |> Seq.windowed n
+        |> Seq.map f
+        |> Seq.append (Seq.init (n-1) (fun _ -> 0.0))
 
   /// <summary>This function is a simplified version of the filter() function
   /// in R. </summary>
@@ -91,32 +65,23 @@ module Signal =
   /// w0x1+w1x2+...+wnx(n+1) ...</returns>
   ///
   let filter w lst =
-    //
-    // Helper function
-    // i - keep track of the first n iterations, when the queue is not full
-    // q - a queue to keep track of the last n elements
-    // total - current total of all the elements in q
-    // acc - the result
-    //
-    let m = List.length w
-    let w' = List.rev w
-    let rec filter' n w lst i q acc =
-      match lst with
-      | [] -> List.rev acc
-      | h::t ->
-          // q isn't full yet, keeping pushing
-          let q' = if i < n then (h::q) else
-                    // q is full, pop at the end and push in the front
-                    h::(List.rev (List.tail (List.rev q)))
-          let tmp = if i >= n - 1 then (dot w q') else 0.0
-          let n' = if i < n then i+1 else n
-          filter' n w t n' q' (tmp::acc)
-    filter' m w' lst 0 [] []
+    filterByFunc (Seq.length w) (dot w) lst
+
+  /// <summary>This function calculates the simple moving average of a list of
+  /// float values</summary>
+  /// <param name="n">Size of the moving window</param>
+  /// <param name="lst">The list of float values</param>
+  /// <returns>The first n-1 values will be 0. The values onwards are the
+  /// moving average calculated at the time point n</returns>
+  ///
+  let movingAverage n lst =
+    filterByFunc n (Seq.average<float>) lst |> Seq.toList
 
   /// <summary>Calculates n day RSI given a list of bars</summary>
   /// <param name="n">number of days</param>
   /// <param name="prices">list of QuantFin.Data.Bars</param>
   /// <returns>list of RSI with the first n entries being zero</returns>
+  ///
   let rsi n (prices: QuantFin.Data.Bar list) =
     let ma = movingAverage n
     prices
@@ -124,6 +89,7 @@ module Signal =
      |> List.map (fun x->x.c)
      // calculate differences between adjacent terms
      |> filter [-1.0; 1.0]
+     |> Seq.toList
      // create list of pair (u, d)
      |> List.map (fun x->if x < 0.0 then (0.0, -x) else (x, 0.0))
      // combine into list of u and list of d
@@ -140,15 +106,17 @@ module Signal =
   /// <param name="n">Size of the moving window</param>
   /// <param name="prices">list of bars</param>
   /// <returns>list of floats</returns>
+  ///
   let sma n (prices: QuantFin.Data.Bar list) =
     let ma = movingAverage n
-    prices |> List.map (fun x->x.c) |> ma
+    prices |> List.map (fun x->x.c) |> ma |> Seq.toList
 
   /// <summary>Calculates different types of technical indicators given a list
   /// of bars </summary>
   /// <param name="t">the technical indicator of type TechnicalIndicator</param>
   /// <param name="prices">the list of price bars</param>
   /// <returns>list of floats</returns>
+  ///
   let calcTI t (prices: QuantFin.Data.Bar list) =
     match t with
     | Rsi n -> rsi n prices
@@ -166,6 +134,7 @@ module Signal =
   /// <param name="prices">the list of price bars</param>
   /// <returns>list of tuple of price bar and float which is the value of the
   /// technical indicator calculated</returns>
+  ///
   let augment ti prices =
     calcTI ti prices |> List.zip prices
 
@@ -174,6 +143,7 @@ module Signal =
   /// <param name="prices">list of price bars to process</param>
   /// <returns>optional pair of System.DateTime indicating the start time and
   /// end time of the pattern if found</returns>
+  ///
   let isBearishEngulf (prices: (QuantFin.Data.Bar * float) list) n =
     match prices with
     | (h1, rsi1)::(h2, rsi2)::_ ->
@@ -182,6 +152,11 @@ module Signal =
           Some (h1.d, h2.d, rsi1, rsi2, n+2) else None
     | _ -> None
 
+  /// <summary>convenience function to calculate the n-day rsi given a list
+  /// of QuantFin.Data.Bar</summary>
+  /// <param name="n">number of days</param>
+  /// <returns>a function which calculates the n-day rsi given a list of
+  /// QuantFin.Data.Bar</returns>
   let augmentRsi n =
     fun prices -> augment (Rsi n) prices
 
