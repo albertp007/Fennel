c1fcd5696fc48b498ed040020911eb02c2c8de76
Albert Pang <albert.pang@me.com>
2015-08-11 21:32:51 +0800

    Changed EWrapperImpl to be a subtype of EWrapper rather than a concrete anonymous instance of it Changed printf to printfn Added a simple test case to test connection

--- IBClient.fs
+++ IBClient.fs
@@ -4,173 +4,191 @@ module IBClient =
 
   open IBApi
 
-  let EWrapperImpl = {
-    new EWrapper with
+  /// <summary>Dummy implementation of the EWrapper interface </summary>
+  ///
+  type EWrapperImpl() = 
+    interface EWrapper with
+
       member this.accountDownloadEnd(account: string) =
-        printf "%s" account
+        printfn "%s" account
 
       member this.accountSummary(reqId, account, tag, value, currency) =
-        printf "accountSummary(%d)" reqId
+        printfn "accountSummary(%d)" reqId
 
       member this.accountSummaryEnd(reqId) =
-        printf "accountSummaryEnd %d" reqId
+        printfn "accountSummaryEnd %d" reqId
 
       member this.bondContractDetails(reqId, contract) =
-        printf "bondContractDetails(%d) %A" reqId contract
+        printfn "bondContractDetails(%d) %A" reqId contract
 
       member this.commissionReport(commissionReport) =
-        printf "commissionReport %A" commissionReport
+        printfn "commissionReport %A" commissionReport
 
       member this.connectionClosed() =
-        printf "connectionClosed"
+        printfn "connectionClosed"
 
       member this.contractDetails(reqId, contractDetails) =
-        printf "contractDetails(%d) %A" reqId contractDetails
+        printfn "contractDetails(%d) %A" reqId contractDetails
 
       member this.contractDetailsEnd(reqId) =
-        printf "contractDetailsEnd(%d)" reqId
+        printfn "contractDetailsEnd(%d)" reqId
 
       member this.currentTime(time) =
-        printf "currentTime %d" time
+        printfn "currentTime %d" time
 
       member this.deltaNeutralValidation(reqId, underComp) =
-        printf "deltaNeutralValidation(%d) %A" reqId underComp
+        printfn "deltaNeutralValidation(%d) %A" reqId underComp
 
       member this.displayGroupList(reqId, group) =
-        printf "displayGroup(%d) %s" reqId group
+        printfn "displayGroup(%d) %s" reqId group
 
       member this.displayGroupUpdated(reqId, group) =
-        printf "displayGroupUpdated(%d) %s" reqId group
+        printfn "displayGroupUpdated(%d) %s" reqId group
 
       member this.error(id, errorCode, errMsg) =
-        printf "error %d %d %s" id errorCode errMsg
+        printfn "error %d %d %s" id errorCode errMsg
 
       member this.error(msg) =
-        printf "error %s" msg
+        printfn "error %s" msg
 
       member this.error(e: exn) =
-        printf "error %s" e.Message
+        printfn "error %s" e.Message
 
       member this.execDetails(reqId, contract, execution) =
-        printf "%d %A %A" reqId contract execution
+        printfn "%d %A %A" reqId contract execution
 
       member this.execDetailsEnd(reqId) =
-        printf "execDetailsEnd(%d)" reqId
+        printfn "execDetailsEnd(%d)" reqId
 
       member this.fundamentalData(reqId, data) =
-        printf "fundamental data(%d) %s" reqId data
+        printfn "fundamental data(%d) %s" reqId data
 
       member this.historicalData(reqId, date, o, h, l, c, volume, count,
-                                 vwap, hasGaps) =
-        printf "historicalData(%d) %s %f %f %f %f %d %d %f %b" reqId date o h
-          l c volume count vwap hasGaps
+                                  vwap, hasGaps) =
+        printfn "historicalData(%d) %s %f %f %f %f %d %d %f %b" reqId date o h
+              l c volume count vwap hasGaps
 
       member this.historicalDataEnd(reqId, s, e) =
-        printf "historicalDataEnd(%d) %s %s" reqId s e
+        printfn "historicalDataEnd(%d) %s %s" reqId s e
 
       member this.managedAccounts(accountsList) =
-        printf "%s" accountsList
+        printfn "%s" accountsList
 
       member this.marketDataType(reqId, marketDataType) =
-        printf "marketData(%d) %d" reqId marketDataType
+        printfn "marketData(%d) %d" reqId marketDataType
 
       member this.nextValidId(orderId) =
-        printf "nextValidId %d" orderId
+        printfn "nextValidId %d" orderId
 
       member this.openOrder(orderId, contract, order, orderState) =
-        printf "openOrder %d %A %A %A" orderId contract order orderState
+        printfn "openOrder %d %A %A %A" orderId contract order orderState
 
       member this.openOrderEnd() =
-        printf "openOrderEnd"
+        printfn "openOrderEnd"
 
       member this.orderStatus(orderId, status, filled, remaining, avgPrice,
                               permId, parentId, lastPrice, clientId,
                               whyHeld) =
-        printf "orderStatus %d %s %d %d %f %d %d %f %d %s" orderId status
+        printfn "orderStatus %d %s %d %d %f %d %d %f %d %s" orderId status
           filled remaining avgPrice permId parentId lastPrice clientId whyHeld
 
       member this.position(account, contract, pos, avgCost) =
-        printf "position %s %A %d %f" account contract pos avgCost
+        printfn "position %s %A %d %f" account contract pos avgCost
 
       member this.positionEnd() =
-        printf "positionEnd"
+        printfn "positionEnd"
 
       member this.realtimeBar(reqId, time, o, h, l, c, v, vwap, count) =
-        printf "realtimeBar(%d) %d %f %f %f %f %d %f %d" reqId time
+        printfn "realtimeBar(%d) %d %f %f %f %f %d %f %d" reqId time
           o h l c v vwap count
 
       member this.receiveFA(faDataType, faXmlData) =
-        printf "receiveFA %d %s" faDataType faXmlData
+        printfn "receiveFA %d %s" faDataType faXmlData
 
       member this.scannerData(reqId, rank, contractDetails, distance,
                               benchmark, projection, legStr) =
-        printf "scannerData(%d) %d %A %s %s %s %s" reqId rank contractDetails
+        printfn "scannerData(%d) %d %A %s %s %s %s" reqId rank contractDetails
           distance benchmark projection legStr
 
       member this.scannerDataEnd(reqId) =
-        printf "scannerDataEnd(%d)" reqId
+        printfn "scannerDataEnd(%d)" reqId
 
       member this.scannerParameters(xml) =
-        printf "scannerParameters %s" xml
+        printfn "scannerParameters %s" xml
 
       member this.tickEFP(tickerId, tickType, basisPoints, formattedBasisPoints,
                           impliedFuture, holdDays, futureExpiry, dividendImpact,
                           dividendsToExpiry) =
-        printf "tickEEP %d %d %f %s %f %d %s %f %f" tickerId tickType
+        printfn "tickEEP %d %d %f %s %f %d %s %f %f" tickerId tickType
           basisPoints formattedBasisPoints impliedFuture holdDays futureExpiry
           dividendImpact dividendsToExpiry
 
       member this.tickGeneric(tickerId, field, value) =
-        printf "tickGeneric %d %d %f" tickerId field value
+        printfn "tickGeneric %d %d %f" tickerId field value
 
       member this.tickOptionComputation(tickerId, field, impliedVolatility,
                                         delta, optPrice, pvDividend, gamma,
                                         vega, theta, undPrice) =
-        printf "tickOptionComputation %d %d %f %f %f %f %f %f %f %f"
+        printfn "tickOptionComputation %d %d %f %f %f %f %f %f %f %f"
           tickerId field impliedVolatility delta optPrice pvDividend gamma
           vega theta undPrice
 
       member this.tickPrice(tickerId, field, price, canAutoExecute) =
-        printf "tickPrice %d %d %f %d" tickerId field price canAutoExecute
+        printfn "tickPrice %d %d %f %d" tickerId field price canAutoExecute
 
       member this.tickSize(tickerId, field, size) =
-        printf "tickSize %d %d %d" tickerId field size
+        printfn "tickSize %d %d %d" tickerId field size
 
       member this.tickSnapshotEnd(tickerId) =
-        printf "tickSnapshotEnd %d" tickerId
+        printfn "tickSnapshotEnd %d" tickerId
 
       member this.tickString(tickerId, field, value) =
-        printf "tickString %d %d %s" tickerId field value
+        printfn "tickString %d %d %s" tickerId field value
 
       member this.updateAccountTime(timestamp) =
-        printf "updateAccountTime %s" timestamp
+        printfn "updateAccountTime %s" timestamp
 
       member this.updateAccountValue(key, value, currency, accountName) =
-        printf "updateAccountValue %s %s %s %s" key value currency accountName
+        printfn "updateAccountValue %s %s %s %s" key value currency accountName
 
       member this.updateMktDepth(tickerId, position, operation, side, price,
-                                 size) =
-        printf "updateMktDepth %d %d %d %d %f %d" tickerId position operation
+                                  size) =
+        printfn "updateMktDepth %d %d %d %d %f %d" tickerId position operation
           side price size
 
       member this.updateMktDepthL2(tickerId, position, marketMaker, operation,
                                    side, price, size) =
-        printf "updateMktDepthL2 %d %d %s %d %d %f %d" tickerId position
+        printfn "updateMktDepthL2 %d %d %s %d %d %f %d" tickerId position
           marketMaker operation side price size
 
       member this.updateNewsBulletin(msgId, msgType, msg, origExchange) =
-        printf "updateNewsBulletin %d %d %s %s" msgId msgType msg origExchange
+        printfn "updateNewsBulletin %d %d %s %s" msgId msgType msg origExchange
 
       member this.updatePortfolio(contract, position, marketPrice, marketValue,
                                   averageCost, unrealizedPNL, realizedPNL,
                                   accountName) =
-        printf "updatePortfolio %A %d %f %f %f %f %f %s" contract position
+        printfn "updatePortfolio %A %d %f %f %f %f %f %s" contract position
           marketPrice marketValue averageCost unrealizedPNL realizedPNL
           accountName
 
       member this.verifyCompleted(isSuccessful, errorText) =
-        printf "verifyCompleted %b %s" isSuccessful errorText
+        printfn "verifyCompleted %b %s" isSuccessful errorText
 
       member this.verifyMessageAPI(apiData) =
-        printf "verifyMessageAPI %s" apiData
-  }
+        printfn "verifyMessageAPI %s" apiData
+
+  /// <summary>Test function for testing connection and request current time
+  /// </summary>
+  ///
+  let testConnect() =
+    let wrapper = EWrapperImpl()
+    let client = EClientSocket(wrapper)
+    client.eConnect("127.0.0.1", 4001, 0, false)
+    client.reqCurrentTime()
+    System.Threading.Thread.Sleep(2000)
+    client.eDisconnect()
+
+  /// <summary>Test suites</summary>
+  ///
+  let runTests() =
+    testConnect()
