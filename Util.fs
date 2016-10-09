c80088349f36b18e2fd0348d6db1b419070ce3cc
Albert Pang <albert.pang@me.com>
2016-10-09 19:15:37 +0800

    Updated to .net 4.6.1.  Added nunit/fsunit/deedle packages.  Updated IBClient with SyncClient.

---
+++ Util.fs
@@ -0,0 +1,38 @@
+﻿namespace QuantFin
+
+module Util =
+
+  open System
+
+  let epsilon = 1.0e-8
+
+  /// <summary>
+  /// Round up a number to arbitrary number of precision
+  /// </summary>
+  /// <param name="x">Number to be rounded up</param>
+  /// <param name="d">Rounding up to the nearest d</param>
+  let roundUp x d =
+    let tmp = x * (1.0/d)
+    if abs(tmp - floor(tmp)) < epsilon then
+      floor(tmp) * d
+    else
+      ceil(tmp) * d
+
+  /// <summary>
+  /// Round down a number to arbitrary number of precision
+  /// </summary>
+  /// <param name="x">Number to be rounded down</param>
+  /// <param name="d">Rounding down to the nearest d</param>
+  let roundDown x d =
+    let tmp = x * (1.0/d)
+    floor(tmp) * d
+
+  /// <summary>
+  /// Prepend a timestamp which is the current time in UTC to a given string
+  /// </summary>
+  let tsLog msg = 
+    let ts = DateTime.UtcNow
+    sprintf "%s - %s" (ts.ToString("dd/MMM/yyyy HH:mm:ss.fff")) msg
+    |> printfn "%s"
+
+    
