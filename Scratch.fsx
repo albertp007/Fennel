47c707380acbad1b1e558b12c0c1feb3776aaaaf
Albert Pang <albert.pang@me.com>
2016-10-17 22:38:18 +0800

    Updated version of FSharp.Data

---
+++ Scratch.fsx
@@ -0,0 +1,13 @@
+﻿#load "packages/Deedle.1.2.5/Deedle.fsx"
+#I @"bin\Debug"
+#I @"packages"
+#r @"QuantFin.dll"
+
+open System
+open Deedle
+open QuantFin.Signal
+open QuantFin.YahooFinance
+open FSharp.Data
+
+let anta = downloadHist "2020.HK"
+let hsi = downloadHist "2800.HK"
